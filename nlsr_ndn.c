4887368fad479340af3632b48d22a54055a054a6
akmhoque <akmhoque@gmail.com>
2012-08-13 14:07:21 -0500

    Exclude in Interest Sending

--- nlsr_ndn.c
+++ nlsr_ndn.c
@@ -157,14 +157,6 @@ process_incoming_interest_lsdb(struct ccn_closure *selfp, struct ccn_upcall_info
 {
 	printf("process_incoming_interest_lsdb called \n");
 	
-	int i,m;
-	//struct upcalldata *data=selfp->data;
-
-	//m=data->n_excl;
-	//for(i=0; i<m; i++)
-		printf("%s ",ccn_charbuf_as_string(selfp->data));
-
-
 }
 
 int
