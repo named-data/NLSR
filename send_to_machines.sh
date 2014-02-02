693c9708003bd907d1596a8764f5e3b33f4f6494
akmhoque <akmhoque@gmail.com>
2014-02-01 23:56:43 -0600

    Clean waf scripts

--- send_to_machines.sh
+++
@@ -1,7 +0,0 @@
-#scp -r NLSR0.0/*  ndnuser@pollux.cs.memphis.edu:/NLSR_TEST/NLSR0.0/
-#scp -r NLSR0.0/*  ndnuser@maia.cs.memphis.edu:/NLSR_TEST/NLSR0.0/
-#scp -r NLSR0.0/*  ndnuser@altair.cs.memphis.edu:/NLSR_TEST/NLSR0.0/
-
-rsync -a -r --progress *.*pp  ndnuser@pollux.cs.memphis.edu:/home/ndnuser/NLSR_CPP/
-rsync -a -r --progress *.*pp  ndnuser@altair.cs.memphis.edu:/home/ndnuser/NLSR_CPP/
-rsync -a -r --progress *.*pp  ndnuser@maia.cs.memphis.edu:/home/ndnuser/NLSR_CPP/
