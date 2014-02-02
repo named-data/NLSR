2e963a0c32f4246d50c24c550ed315af1db4decd
akmhoque <akmhoque@gmail.com>
2014-02-01 23:55:20 -0600

    Adding waf make script

---
+++ build/c4che/build.config.py
@@ -0,0 +1,2 @@
+version = 0x1070d00
+tools = [{'tool': 'ar', 'tooldir': None, 'funs': None}, {'tool': 'cxx', 'tooldir': None, 'funs': None}, {'tool': 'g++', 'tooldir': None, 'funs': None}, {'tool': 'compiler_cxx', 'tooldir': None, 'funs': None}, {'tool': 'boost', 'tooldir': None, 'funs': None}, {'tool': 'gnu_dirs', 'tooldir': None, 'funs': None}]
